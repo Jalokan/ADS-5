3b7e183a62547e9c40e719aba065338fb847c621
Jalokan <150829310+Jalokan@users.noreply.github.com>
2024-04-14 14:42:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,6 +3,46 @@
 #include <map>
 #include "tstack.h"
 
+int alphabet(char c) {
+    switch (c) {
+    case('1'): return 1;
+    case('2'): return 2;
+    case('3'): return 3;
+    case('4'): return 4;
+    case('5'): return 5;
+    case('6'): return 6;
+    case('7'): return 7;
+    case('8'): return 8;
+    case('9'): return 9;
+    case('0'): return 0;
+    }
+    return -1;
+}
+
+bool Cheching_for_a_Num(char a) {
+    return (a >= '0' && a <= '9');
+}
+
+int get_operator_priority(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    } else if (op == '*' || op == '/') {
+        return 2;
+    } else {
+        return 0;
+    }
+}
+
+int ifOperator(char c) {
+    switch (c) {
+    case('+'): return 1;
+    case('-'): return 2;
+    case('*'): return 3;
+    case('/'): return 4;
+    }
+    return 0;
+}
+
 std::string infx2pstfx(std::string inf) {
     TStack<char, 100> s;
     std::string result = "";
